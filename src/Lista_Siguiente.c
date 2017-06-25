dcc4c262afa4f118241e69ca43fe4099be9ada35
AnibalG <anibalgamboac@gmail.com>
2017-06-25 17:56:08 -0500

    funcional

--- src/Lista_Siguiente.c
+++ src/Lista_Siguiente.c
@@ -7,9 +7,8 @@
 ElementoLista *Lista_Siguiente(ListaEnlazada *lista, ElementoLista *elemento){
 	if ( (lista!=NULL) && (elemento!=NULL) && (lista->numeroElementos!=0) ){
 		ElementoLista *ancla = &(lista -> ancla);
-		ElementoLista *help = Lista_Buscar(lista,elemento->objeto);
-		if(help!=NULL && (help->siguiente != ancla)){
-			return help->siguiente;
+		if(elemento->siguiente != ancla){
+			return elemento->siguiente;
 		}
 
 	}
